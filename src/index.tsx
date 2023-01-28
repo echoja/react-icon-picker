950e4b304124f712a37fbc9a130ed3e9dcf02716
Kim Tae Hoon <eszqsc112@gmail.com>
2023-01-28 11:17:51 +0900

    add example component

---
+++ src/index.tsx
@@ -0,0 +1,23 @@
+"use client";
+
+import React from "react";
+
+export interface IReactIconPickerProps {
+  icons: string[];
+  onSelect: (icon: string) => void;
+}
+
+const ReactIconPicker: React.FC<IReactIconPickerProps> = ({
+  icons,
+  onSelect,
+}) => {
+  return (
+    <ul>
+      {icons.map((icon) => (
+        <li onClick={() => onSelect(icon)}>{icon}</li>
+      ))}
+    </ul>
+  );
+};
+
+export default ReactIconPicker;
